29bb470dd6dc9b1949acad12627564ed402cb223
Idrishkaidawala <kaidawalaidrish9@gmail.com>
2025-01-18 14:03:33 -0500

    This is my Week01

---
+++ Week01/lab/coding questions/lab1.py
@@ -0,0 +1,27 @@
+# Sample Coding Questions 01 Week 01
+# Idrish Kaidawala
+
+
+#Defining Variable
+
+array_variable = [1,4,7,9]
+
+#Order of Operation
+
+a = 1
+b = 2
+c = 3
+d = 4
+
+e = (((a - (b ** c)) // d) + (a % c))
+print(e)
+
+#Formating
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+#Common Function
+
+userAge = int(input("Enter your age: ")) + 22
+print("Now showing the shop items filtered by age: {}".format(userAge))
+
